758fafe9450b008757966b818f82324d85ba74a5
Jay Kickliter <jay.kickliter@gmail.com>
2014-08-31 10:25:52 -0600

    Changed example, since multirate filters now scale taps by interpolation.

--- examples/DAT to CD.jl
+++ examples/DAT to CD.jl
@@ -1,7 +1,7 @@
 # Borrowed from http://www.mathworks.com/help/signal/ref/upfirdn.html
 
+using  Winston
 import Multirate
-import Winston
 
 sampleRate    = 48000
 interpolation = 147
@@ -9,10 +9,10 @@ decimation    = 160
 ratio         = interpolation  // decimation
 Nt            = 24*interpolation
 
-h = Multirate.firdes( Nt, 0.5/decimation, Multirate.kaiser, beta = 7.8562  ) .* interpolation
+h = Multirate.firdes( Nt, 0.5/interpolation, Multirate.kaiser, beta = 7.8562  )
 
-n = 0:10239                    
-x = sin(2*pi*1e3/sampleRate*n) 
+n = 0:10239
+x = sin(2*pi*1e3/sampleRate*n)
 y = Multirate.filt( h, x, ratio )
 
 tOriginal = n[1:49]./sampleRate
@@ -20,4 +20,7 @@ xOriginal = x[1:49]
 tResamp   = n[1:45]./(sampleRate*ratio)
 xResamp   = y[13:57]
 
-plot(tOriginal, xOriginal, "b:", tResamp, xResamp, "r:")
+display( stem( tOriginal, xOriginal, "b." ) )
+hold( true )
+display( stem( tResamp, xResamp, "r." ) )
+hold( false )
