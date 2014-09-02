a0acb7b55fdc3b689455891161e1a50408d143b0
Jay Kickliter <jay.kickliter@gmail.com>
2014-09-01 19:13:55 -0600

    function renamed

--- examples/Comparison.jl
+++ examples/Comparison.jl
@@ -9,7 +9,7 @@ numTaps       = 24*interpolation
 x             = rand( 1_000_000 )
 h             = firdes( numTaps, 0.5/interpolation, Multirate.kaiser, beta = 7.8562  )
 
-function bruteresample{T}( h::Vector{T}, x::Vector{T}, ratio::Rational{Int} )
+function naiveresampler{T}( h::Vector{T}, x::Vector{T}, ratio::Rational{Int} )
     upfactor   = num( ratio )
     downfactor = den( ratio )
     xStuffed   = zeros( T, length(x) * upfactor )
@@ -22,5 +22,5 @@ function bruteresample{T}( h::Vector{T}, x::Vector{T}, ratio::Rational{Int} )
     y = [ yInterpolated[n] for n = 1:downfactor:length( yInterpolated ) ]
 end
 
-@time y = bruteresample( h, x, 147//160 );
+@time y = naiveresampler( h, x, 147//160 );
 @time y = filt( h, x, ratio );
