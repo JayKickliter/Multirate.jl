46bbd0033dceaaa11c20e61245034da95eea61a1
Jay Kickliter <jay@kickliter.com>
2014-09-07 15:44:36 -0600

    Cleaned up temporary test

--- test/Arbitrary Resampler Scratchpad.jl
+++ test/Arbitrary Resampler Scratchpad.jl
@@ -5,33 +5,34 @@ using Base.Test
 # http://cnx.org/contents/30153d7b-7130-4dd3-89af-53125f3891e5@10
 
 Nfilters        = 32;
-resampleRate    = 0.95
+resampleRate    = 9.999;
 cutoffFreq      = 0.45;
 transitionWidth = 0.05;
 (hLen, β)       = MR.kaiserlength( transitionWidth, samplerate = Nfilters );
 hLen            = iceil(  hLen/Nfilters  ) .* Nfilters;
 h               = MR.firdes( hLen, cutoffFreq, MR.kaiser, samplerate = 32, beta = β ) .* Nfilters;
-
+t               = linspace( 0, 40*2*pi, 100)
+x               = sin( t )
 
 arb_resampler  = MR.FIRFilter( h, resampleRate );
 arb_piecewise_resampler = MR.FIRFilter( h, resampleRate );
-
-x = [ 1.0:int(rand(97:110)) ];
 
 @printf( "\n\n\nNaive arbitrary resampling\n" )
 @time naivere_result  = MR.naivefilt( h, x, resampleRate, Nfilters);
 
 @printf( "\n\n\nNormal arbitrary resampling\n" )
-@time arb_result      = MR.filt( arb_resampler, x );
+@time arbitrary_result      = MR.filt( arb_resampler, x );
 
 
 @printf( "\n\n\nPiecewise arbitrary resampling\n" )
+pivot_point = ifloor( length(x)/4 )
 @time begin
-    arb_piecewise_result = MR.filt( arb_piecewise_resampler, x[1:10] );
-    append!( arb_piecewise_result, MR.filt( arb_piecewise_resampler, x[11:end] ) );
+    piecewise_result = MR.filt( arb_piecewise_resampler, x[1:pivot_point] );
+    append!( piecewise_result, MR.filt( arb_piecewise_resampler, x[pivot_point+1:end] ) );
 end
 
-display( [ naivere_result arb_result arb_piecewise_result ] )
+display( [ [1:length(naivere_result)] naivere_result arbitrary_result  piecewise_result naivere_result.-arbitrary_result naivere_result.-piecewise_result ] )
 
-@test areApprox( naivere_result, arb_result )
-@test areApprox( naivere_result, arb_piecewise_result )
+@test areApprox( arbitrary_result,  piecewise_result )
+@test areApprox( naivere_result,    arbitrary_result )
+@test areApprox( naivere_result,    piecewise_result )
