53711df4d20e941e0af7581faf5737e448732566
Jay Kickliter <jay.kickliter@gmail.com>
2014-09-30 19:46:30 -0600

    Betterized™ readability.

--- test/farrowtest.jl
+++ test/farrowtest.jl
@@ -1,19 +1,24 @@
 using Multirate
 
+# Filter tap parameters
 Th              = Float64
 Tx              = Float64
 N𝜙              = 32
 resamplerate    = 32/33.5
-cutofffreq      = 0.45
-transitionwidth = 0.05
+ƒcutoff         = 0.45
+transitionwidth = 0.1
 (hLen, β)       = kaiserlength( transitionwidth, samplerate = N𝜙 )
 hLen            = iceil(  hLen/N𝜙  ) .* N𝜙
-h               = Multirate.firdes( hLen, cutofffreq, DSP.kaiser, samplerate = N𝜙, beta = β ) .* N𝜙
+h               = Multirate.firdes( hLen, ƒcutoff, DSP.kaiser, samplerate = N𝜙, beta = β ) .* N𝜙
 h               = convert( Vector{Th}, h )
-polyorder       = 6
+
+# Filter instantiation
+polyorder       = 5
 x               = rand( Tx, 50  )
 arbfilt         = FIRFilter( h, resamplerate, N𝜙 )
 farrowfilt      = FIRFilter( h, resamplerate, N𝜙, polyorder )
+
+# Results
 yarb            = filt( arbfilt, x)
 yfarrow         = filt( farrowfilt, x )
 diff_arb_farrow = yarb.-yfarrow
