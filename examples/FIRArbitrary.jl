b8d95035a48b39b92211659c452634e2b2906ed7
Jay Kickliter <jay@kickliter.com>
2014-10-01 17:26:07 -0600

    Added resampling example

---
+++ examples/FIRArbitrary.jl
@@ -0,0 +1,25 @@
+using Multirate
+using PyPlot
+
+N𝜙              = 32
+tapsPer𝜙        = 10
+resampleRate    = 3.14
+xƒ              = 0.1
+Nx              = 25
+x               = cos(2*pi*xƒ*(0:Nx-1))
+# x             = zeros( Nx); x[1] = 1.0
+cutoffFreq      = min( 0.45, resampleRate )
+transitionWidth = 0.05
+hLen            = tapsPer𝜙*N𝜙
+h               = firdes( hLen, cutoffFreq, DSP.kaiser, samplerate = N𝜙, beta = 5 ) .* N𝜙
+myfilter        = FIRFilter( h, resampleRate, N𝜙 )
+setphase!( myfilter, 1.5 )
+y               = filt( myfilter, x )
+tx              = [0:length(x)-1]
+ty              = [0:length(y)-1]./resampleRate
+
+
+stem( tx, x, "r" )
+hold(true)
+stem( ty, y )
+hold(false)
