7f068b01ca576f7f49997a76c97de439083c78f9
Jay Kickliter <jay@kickliter.com>
2014-10-05 18:57:09 -0600

    Added an interactive farrow filter example

---
+++ examples/Interactive Farrow Example.jl
@@ -0,0 +1,41 @@
+using Multirate
+using DSP
+using GtkInteract
+using Winston
+
+N𝜙                = 32                                           # Number of polyphase partitions
+tapsPer𝜙          = 12                                            # N𝜙 * tapsPer𝜙 will be the length of out protoyTimepe filter taps
+ƒsIn              = 1.0                                          # Input sample rate
+ƒsOut             = π                                            # Input sample rate
+resampleRatio     = ƒsOut/ƒsIn
+polyorder         = 4                                            # Our taps will tranformed into
+xƒ1               = 0.125                                        # First singal frequency
+xƒ2               = 0.3                                          # Second signal frequency
+xLen              = 20                                           # Number of signal samples
+xTime             = [0:xLen-1]                                   # Signal time vector
+x                 = cos(2*pi*xƒ1*xTime)
+x                 = x + 0.5sin(2*pi*xƒ2*xTime*pi)                # Create the two signals and add them
+x                 = x + cos(0.1*xTime)
+cutoffFreq        = min( 0.45/N𝜙, resampleRatio/N𝜙 )             # N𝜙 is also the integer interpolation, so set cutoff frequency accordingly
+hLen              = tapsPer𝜙*N𝜙                                  # Tintal number of filter taps
+h                 = firdes( hLen, cutoffFreq, DSP.kaiser ) .* N𝜙 # Generate filter taps and scale by polyphase interpolation (N𝜙)
+myfilter          = FIRFilter( h, resampleRatio, N𝜙, polyorder ) # Construct a FIRFilter{FIRFarrow} object
+kernel            = myfilter.kernel
+
+
+
+
+δfilter           = (hLen-1)/(2*N𝜙)                              # The time delay (at output rate) caused by the filtering process
+
+
+@manipulate for δout = 0.0:0.1:floor(xTime[end]-δfilter), out=:plot
+    (phase,throwaway)    = modf(δfilter+δout)                           # Number of samples (at output rate) to throw away to meet time offset 𝜙
+    reset(myfilter)
+    kernel.inputDeficit  = throwaway+1
+    setphase( myfilter, phase )
+    y     = filt( myfilter, x )
+    yTime = (0.0:length(y)-1)/resampleRatio+δout       # Create y time vector. Accout for filter delay so the plots line up    
+    p     = plot( xTime, x, "b-*", yTime, y, "r-*" )
+
+    push!(out, p)
+end
